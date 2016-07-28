cd4dbc6d7201902b9869970d61d776547d3e143c
Scott W. Bradley <scottwb@scottwb.net>
2016-07-28 13:14:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Usage:
+
+  * `git clone`
+  * `vagrant up`
+  * Hit http://htaccess.test/whatever/path/you/want
+  * See variables in web page
+  * Edit `.htaccess` in this directory to experiment
+  * Refresh browser
+  * Repeat
---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head><title>Apache Test</title></head>
+  <body>
+    <h1>Apache Test</h1>
+
+    <br/><br/>
+    <b>ENV:</b>
+    <pre>
+      <?php print_r($_ENV); ?>
+    </pre>
+
+    <br/><br/>
+    <b>SERVER:</b>
+    <pre>
+      <?php print_r($_SERVER); ?>
+    </pre>
+
+  </body>
+</html>
